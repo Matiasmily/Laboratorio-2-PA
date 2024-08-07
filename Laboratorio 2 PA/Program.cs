d26cc99aea457bd049ff6c4bb50e2879746d1fb9
Mily Matías <158805363+Matiasmily@users.noreply.github.com>
2024-08-07 09:20:30 -0600

    se añadieron los menús

--- Laboratorio 2 PA/Program.cs
+++ Laboratorio 2 PA/Program.cs
@@ -1,1 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+ void MostrarMenu()
+{
+    Console.Clear();
+    Console.WriteLine("--- HOTEL BUENA VISTA ---");
+    Console.WriteLine("1. Agregar habitación");
+    Console.WriteLine("2. Eliminar Habitación");
+    Console.WriteLine("3. Mostrar Habitaciones");
+    Console.WriteLine("4. Asignar Habitación al Cliente");
+    Console.WriteLine("5. Liberar Habitación");
+    Console.WriteLine("4. Salir");
+    Console.Write("Ingrese una opción: ");
+}
+void MenuHabitación()
+{
+    Console.Clear();
+    Console.WriteLine("--- MENÚ HABITACIONES ---");
+    Console.WriteLine("1. Habitación Simple");
+    Console.WriteLine("2. Habitación Doble");
+    Console.WriteLine("3. Suite");
+    Console.WriteLine("4. Habitación Deluxe");
+    Console.WriteLine("5. Regresar al menú anterior");
+    Console.Write("Ingrese una opción: ");
+}
